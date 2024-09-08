a253e65413733f294fa06de94f2c779bc5a7ff04
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-07 18:44:12 -0600

    productoLLVG creado

---
+++ LLVG20240907G5/Modelos/EN/CRMContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LLVG20240907G5.Modelos.EN
+{
+    public class CRMContext:DbContext
+    {
+        public CRMContext(DbContextOptions<CRMContext> options) : base(options)
+        {
+        }
+        public DbSet<ProductoLLVG> ProductoLLVG { get; set; }
+    }
+}
---
+++ LLVG20240907G5/Modelos/EN/ProductoLLVG.cs
@@ -0,0 +1,10 @@
+﻿namespace LLVG20240907G5.Modelos.EN
+{
+    public class ProductoLLVG
+    {
+        public int id { get; set; }
+        public string NombreLLVG { get; set; }
+        public string DescripcionLLVG { get; set; }
+        public decimal PrecioLLVG { get; set; }
+    }
+}
